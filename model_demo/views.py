0befc156e069eea9cd1077504621a9b9eec49623
vivian <xxxxx@xxx.com>
2020-06-02 01:11:09 +0800

    0602

--- model_demo/views.py
+++ model_demo/views.py
@@ -1,5 +1,5 @@
 import random
-
+from django.core.paginator import Paginator
 from django.db import transaction
 from django.http import HttpResponse, JsonResponse
 from django.views import View
@@ -24,41 +24,65 @@ def random_phone(request):
     return HttpResponse("1{}{}{}".format(second, third, suffix))
 
 
+
+
 class DataBase(View):
     def get(self, request):
-        student = Student.objects.all()
+        # dict_list = []
+        data = request.GET
+        print(data["pagesize"])
+        # for i in data:
+        #     dict_list.append({i:data[i]})
+        # print(dict_list)
+        # student = Student.objects.filter(**dict_list[0])  # Paginator(Student.objects.filter(**dict_list[0]),per_page=10)
+
+        # p = Paginator(Student.objects.all().values().order_by('id'), per_page=data["pagenum"]) # pagenum 设置每页展示多少条数据
+        # data_list = []
+        # for i in p.get_page(data["pagesize"]).object_list: # pagesize 查看第几页的数据
+        #     data_list.append(i)
+
+        p = Paginator(Student.objects.filter(s_name=data['name']).values().order_by("id"), per_page=data["pagenum"])
         data_list = []
-        for i in student:
-            d={
-                "id":i.id,
-                "name":i.s_name,
-                "sex":i.s_sex,
-                "phone":i.s_phone,
-            }
-            data_list.append(d)
-        return JsonResponse({"code":200,"msg":"查询成功","data":data_list},safe=False)
+        for i in p.get_page(data["pagesize"]).object_list:
+            data_list.append(i)
+
+        # data_list = []
+        # for i in student:
+        #     d={
+        #         "name":i.s_name,
+        #         "sex":i.s_sex,
+        #         "phone":i.s_phone,
+        #         "create_time":i.create_time,
+        #         "update_time":i.update_time
+        #     }
+        #     data_list.append(d)
+        return JsonResponse({"code":200,"msg":"查询成功","data":data_list},safe=False) #
 
     def post(self, request):
         student = json.loads(request.body)
         # student_list = []
         # for i in student:
-        #     student_list.append(Student(s_name=i["name"],s_sex=["sex"],s_phone=["phone"]))
-        student_list = [Student(s_name=i["name"],s_sex=i["sex"],s_phone=i["phone"]) for i in student]
+        #     student_list.append(Student(s_name=i["name"],s_sex=i["sex"],s_phone=i["phone"]))
+        student_list = [Student(s_name=i["s_name"],s_sex=i["s_sex"],s_phone=i["s_phone"]) for i in student]
         Student.objects.bulk_create(student_list)
-        return JsonResponse({"code": 200, "masge": "新增成功"})
+        return JsonResponse({"code": 200, "masge": "新增成功","data":student},safe=False)
 
     def put(self, request):
-        students = Student.objects.all()
         data = json.loads(request.body)
+        students = Student.objects.filter(id=data["id"]).values()  # 筛选过滤满足传入的id值的数据
+        data_list = []  # 获取修改的数据存进dict_list
+        for i in students:
+            data_list.append(i)
         # transaction.set_autocommit(False)
-        students.update(**data)
+        students.update(**data)  # 根据传入的json数据,利用魔法传参的方式更新该条数据
         # s.save()  # 修改单个字段值,需要save
         # Student.objects.all().update(s_name="雷阳洪")
         # transaction.commit()  # 提交修改的操作
         # transaction.rollback()  # 如果报错就回滚
-        return JsonResponse({"code": 200, "masge": "修改成功"})
+        return JsonResponse({"code": 200, "masge": "修改成功","data":data_list})
 
     def delete(self, request):
-        Student.objects.all().delete()
-        return JsonResponse({"code": 200, "masge": "删除成功"})
+        data = json.loads(request.body)
+        Student.objects.filter(id=data["id"]).delete()  # 根据传入的id值,删除指定数据的id数据
+        return JsonResponse({"code": 200, "masge": "删除成功","data":data})
 
