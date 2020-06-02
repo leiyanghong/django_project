7636b9dd34fdfab9ea13e948719429139735e76c
雷阳洪 <hongyang.lei@mathartsys.com>
2020-06-02 19:28:21 +0800

    0602

---
+++ app02/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+
+
+# Create your models here.
+class Stu(models.Model):
+    '''
+    max_length:限制字段最大长度
+    null:False 不能为空
+    help_text:备注的文本信息
+    '''
+    s_name = models.CharField('学生姓名', max_length=64, null=False, help_text='学生姓名')
+    # choices:限制字段数据只能为0或者1
+    s_sex = models.CharField("性别", max_length=1, choices=(
+        (0, '男'),
+        (1, '女')
+    ), help_text='性别：0-男；1-女')
+    # PositiveIntegerField:设置正整数的字段类型 0 ～ 2147483647
+    # default:设置字段的默认值为0
+    s_age = models.PositiveIntegerField('年龄', null=False, default=0, help_text='年龄')
+    s_dept = models.CharField("专业", max_length=64, null=False, help_text="专业")
+    '''
+    ManyToManyField():多对多关联 
+    to:设置要关联的表
+    db_table:默认创建第三张表时，数据库中表的名称。
+    related_name:同ForeignKey字段
+    '''
+    course = models.ManyToManyField(to="Course", db_table="stu_course", related_name="course_student")
+
+    class Meta:
+        db_table = 'stu'
+
+
+class StudentInfo(models.Model):
+    s_card = models.CharField("身份证号", max_length=18, null=False, unique=True)
+    s_phone = models.CharField(max_length=11, null=False, unique=True, help_text="手机号")
+    s_addr = models.CharField("家庭住址", max_length=128, help_text="家庭住址")
+    s_id = models.OneToOneField(to='Stu', to_field="id", on_delete=models.CASCADE, db_column='s_id',
+                                related_name="stu_info_stu")
+
+    class Meta:
+        db_table = "student_info"
+
+
+class Course(models.Model):
+    c_name = models.CharField('课程名', max_length=64, null=False, help_text="课程名")
+    t_id = models.ForeignKey(to="Teacher", to_field="id", on_delete=models.CASCADE, related_name='teacher_course',
+                             db_column='t_id', help_text="老师编号")
+
+    class Meta:
+        db_table = "course"
+
+
+class Score(models.Model):
+    s_id = models.ForeignKey(to="Stu", to_field="id", on_delete=models.CASCADE, related_name='stu_score',
+                             db_column='s_id', help_text="学生编号")
+    c_id = models.ForeignKey(to="Course", to_field="id", on_delete=models.CASCADE, related_name='course_score',
+                             db_column='c_id', help_text="学生编号")
+    score = models.PositiveIntegerField("成绩", null=False, default=0, help_text="成绩")
+
+    class Meta:
+        db_table = "score"
+
+
+class Teacher(models.Model):
+    t_name = models.CharField("老师姓名", max_length=64, null=False, help_text="老师姓名")
+
+    class Meta:
+        db_table = "teacher"
--- model_demo/views.py
+++ model_demo/views.py
@@ -1,3 +1,88 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # leiyh
+import random
+from django.core.paginator import Paginator
+from django.db import transaction
+from django.http import HttpResponse, JsonResponse
+from django.views import View
+import json
+from model_demo.models import Student
+
+
+def random_phone(request):
+    # 第二位数字
+    second = [3, 4, 5, 7, 8][random.randint(0, 4)]
+    # 第三位数字
+    third = {
+        3: random.randint(0, 9),
+        4: [5, 7, 9][random.randint(0, 2)],
+        5: [i for i in range(10) if i != 4][random.randint(0, 8)],
+        7: [i for i in range(10) if i not in [4, 9]][random.randint(0, 7)],
+        8: random.randint(0, 9),
+    }[second]
+    # 最后八位数字
+    suffix = random.randint(9999999, 100000000)
+    # 拼接手机号
+    return HttpResponse("1{}{}{}".format(second, third, suffix))
+
+
+class DataBase(View):
+    def get(self, request):
+        # dict_list = []
+        data = request.GET
+        print(data["pagesize"])
+        # for i in data:
+        #     dict_list.append({i:data[i]})
+        # print(dict_list)
+        # student = Student.objects.filter(**dict_list[0])  # Paginator(Student.objects.filter(**dict_list[0]),per_page=10)
+
+        # p = Paginator(Student.objects.all().values().order_by('id'), per_page=data["pagenum"]) # pagenum 设置每页展示多少条数据
+        # data_list = []
+        # for i in p.get_page(data["pagesize"]).object_list: # pagesize 查看第几页的数据
+        #     data_list.append(i)
+
+        p = Paginator(Student.objects.filter(s_name=data['name']).values().order_by("id"), per_page=data["pagenum"])
+        data_list = []
+        for i in p.get_page(data["pagesize"]).object_list:
+            data_list.append(i)
+
+        # data_list = []
+        # for i in student:
+        #     d={
+        #         "name":i.s_name,
+        #         "sex":i.s_sex,
+        #         "phone":i.s_phone,
+        #         "create_time":i.create_time,
+        #         "update_time":i.update_time
+        #     }
+        #     data_list.append(d)
+        return JsonResponse({"code": 200, "msg": "查询成功", "data": data_list}, safe=False)  #
+
+    def post(self, request):
+        student = json.loads(request.body)
+        # student_list = []
+        # for i in student:
+        #     student_list.append(Student(s_name=i["name"],s_sex=i["sex"],s_phone=i["phone"]))
+        student_list = [Student(s_name=i["s_name"], s_sex=i["s_sex"], s_phone=i["s_phone"]) for i in student]
+        Student.objects.bulk_create(student_list)
+        return JsonResponse({"code": 200, "masge": "新增成功", "data": student}, safe=False)
+
+    def put(self, request):
+        data = json.loads(request.body)
+        students = Student.objects.filter(id=data["id"]).values()  # 筛选过滤满足传入的id值的数据
+        data_list = []  # 获取修改的数据存进dict_list
+        for i in students:
+            data_list.append(i)
+        # transaction.set_autocommit(False)
+        students.update(**data)  # 根据传入的json数据,利用魔法传参的方式将字典数据转换成键值数据并更新该条数据
+        # s.save()  # 修改单个字段值,需要save
+        # Student.objects.all().update(s_name="雷阳洪")
+        # transaction.commit()  # 提交修改的操作
+        # transaction.rollback()  # 如果报错就回滚
+        return JsonResponse({"code": 200, "masge": "修改成功", "data": data_list})
+
+    def delete(self, request):
+        data = json.loads(request.body)
+        Student.objects.filter(id=data["id"]).delete()  # 根据传入的id值,删除指定数据的id数据
+        return JsonResponse({"code": 200, "masge": "删除成功", "data": data})
