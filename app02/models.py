f448f130a4e2a4519ea9ff377fd77639ee758c20
vivian <xxxxx@xxx.com>
2020-06-04 21:26:26 +0800

    0604

--- app02/models.py
+++ app02/models.py
@@ -34,6 +34,7 @@ class StudentInfo(models.Model):
     s_card = models.CharField("身份证号", max_length=18, null=False, unique=True)
     s_phone = models.CharField(max_length=11, null=False, unique=True, help_text="手机号")
     s_addr = models.CharField("家庭住址", max_length=128, help_text="家庭住址")
+    # 一对一
     s_id = models.OneToOneField(to='Stu', to_field="id", on_delete=models.CASCADE, db_column='s_id',
                                 related_name="stu_info_stu")
 
@@ -43,6 +44,7 @@ class StudentInfo(models.Model):
 
 class Course(models.Model):
     c_name = models.CharField('课程名', max_length=64, null=False, help_text="课程名")
+    # 多对一
     t_id = models.ForeignKey(to="Teacher", to_field="id", on_delete=models.CASCADE, related_name='teacher_course',
                              db_column='t_id', help_text="老师编号")
 
@@ -51,8 +53,10 @@ class Course(models.Model):
 
 
 class Score(models.Model):
+    # 多对一
     s_id = models.ForeignKey(to="Stu", to_field="id", on_delete=models.CASCADE, related_name='stu_score',
                              db_column='s_id', help_text="学生编号")
+    # 多对一
     c_id = models.ForeignKey(to="Course", to_field="id", on_delete=models.CASCADE, related_name='course_score',
                              db_column='c_id', help_text="学生编号")
     score = models.PositiveIntegerField("成绩", null=False, default=0, help_text="成绩")
